0bd7468a8fe2452083b1a4444d5dbf1a91a766ff
Teresa Le <teresale8@gmail.com>
2022-12-10 01:17:25 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,28 @@ mecha <- read.csv("C:/Users/Teresa - School/Documents/1. U of T SCS - Data Analy
 mecha.model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mecha)
 
 summary(mecha.model)
+
+# Create visualizations for trip analysis 
+
+coil <- read.table("C:/Users/Teresa - School/Documents/1. U of T SCS - Data Analytics Bootcamp/Module 16 - Statistics and R/MechaCar-Statistical-Analysis/Suspension_Coil.csv", sep = ",", header = TRUE)
+
+summary(coil$PSI)
+
+total_summary <- coil %>%
+  summarize(mean = mean(PSI),
+            median = median(PSI),
+            variance = var(PSI),
+            sd = sd(PSI))
+
+lot_summary <- coil %>% 
+  group_by(Manufacturing_Lot)%>% 
+  summarize(mean = mean(PSI),
+            median = median(PSI),
+            variance = var(PSI),
+            sd = sd(PSI))
+
+  
+
+
+
+
